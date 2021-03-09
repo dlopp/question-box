b4bddd18a8d88c8e035468fa8819e4588ae4e86a
Daaiki <daaikii@outlook.jp>
2021-03-09 15:22:39 +0900

    package: :package: eslint-plugin-prettierを削除
    https://blog.ojisan.io/prettier-eslint-cli

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,14 +7,8 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint',
   },
-  extends: [
-    '@nuxtjs',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
-  ],
-  plugins: ['prettier'],
+  extends: ['@nuxtjs', 'prettier', 'prettier/vue', 'plugin:nuxt/recommended'],
+  plugins: [],
   // add your custom rules here
   rules: {},
 }
