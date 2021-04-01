5f15591b0370cafc2587bea697e5cb474c7707ea
Daaiki <daaikii@outlook.jp>
2021-04-01 09:55:49 +0900

    fix: :adhesive_bandage: redirectのパス修正

--- middleware/notAuthenticated.js
+++ middleware/notAuthenticated.js
@@ -2,6 +2,6 @@ export default function ({ store, redirect }) {
   // 認証不要ページ用
   // もし認証済みなら、管理ページへリダイレクト
   if (store.state.auth) {
-    return redirect('/admin')
+    return redirect('/admin/questions')
   }
 }
