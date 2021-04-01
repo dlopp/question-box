eacd661bf812e59c5536a869d03558232c4a3a58
wholemilk <weekender.nyanko@gmail.com>
2021-04-01 14:01:54 +0900

    UI: :art: headerを作成した

---
+++ components/Layouts/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="w-full h-16 bg-white shadow-md">
+    <LayoutsContainer class="h-full flex items-center">
+      <nuxt-link to="/">
+        <img
+          class="inline-block h-8"
+          src="~/assets/images/header.png"
+          alt="dlopp"
+        />
+      </nuxt-link>
+    </LayoutsContainer>
+  </header>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <LayoutsHeader />
     <LayoutsContainer>
       <Nuxt />
     </LayoutsContainer>
