724e30b980ece641d91b2fec4953f59482a1ed1c
Daaiki <daaikii@outlook.jp>
2021-04-01 11:42:32 +0900

    package: :package: Tailwindを拡張する

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,4 +3,24 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
+  purge: [
+    './components/**/*.{vue,js}',
+    './layouts/**/*.{vue,js}',
+    './pages/**/*.{vue,js}',
+    './nuxt.config.js',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        main: '#87DAD6',
+        accent: '#FFE45A',
+      },
+    },
+    screens: {
+      xs: '475px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+    },
+  },
 }
