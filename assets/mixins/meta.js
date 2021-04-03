b256be1ce5c4ad631372ae8242e09c69f1ae6c5f
Daaiki <daaikii@outlook.jp>
2021-04-03 14:50:17 +0900

    feat: :sparkles: meta情報のmixins作成

---
+++ assets/mixins/meta.js
@@ -0,0 +1,23 @@
+export default {
+  head() {
+    return {
+      title: this.meta.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.meta.description,
+        },
+        { hid: 'og:type', property: 'og:type', content: this.meta.type },
+        { hid: 'og:title', property: 'og:title', content: this.meta.title },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: this.meta.description,
+        },
+        { hid: 'og:url', property: 'og:url', content: this.meta.url },
+        { hid: 'og:image', property: 'og:image', content: this.meta.image },
+      ],
+    }
+  },
+}
