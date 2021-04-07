ac86f601af3057aca0d86b3feb97fce442e74575
Daaiki <daaikii@outlook.jp>
2021-04-07 18:12:28 +0900

    package: :package: vue-scrolltoを導入

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,7 +60,7 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [{ src: '~/plugins/vue-scrollto', mode: 'client' }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
