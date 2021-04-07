e54533be901736819db6b1c4ff0df20e6e51a216
Daaiki <daaikii@outlook.jp>
2021-04-07 14:42:41 +0900

    UI: :art: ロゴ左にくっつくの修正

--- components/Layouts/Header.vue
+++ components/Layouts/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="w-full h-16 bg-white shadow-md">
     <LayoutsContainer class="h-full flex items-center">
-      <nuxt-link to="/">
+      <nuxt-link class="ml-5" to="/">
         <img
           class="inline-block h-8"
           src="~/assets/images/header.png"
