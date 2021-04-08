5e630dfd061d43b0174e1b927c5b50a20ba2c5fb
Daaiki <daaikii@outlook.jp>
2021-04-08 19:20:32 +0900

    refactor: :recycle: answersページのtitleテンプレートをnullに

--- pages/answers/_id.vue
+++ pages/answers/_id.vue
@@ -27,14 +27,15 @@ export default {
   },
   head() {
     return {
-      title: this.question.body,
+      titleTemplate: null,
+      title: 'dlopp専用質問箱！',
       meta: [
         {
           hid: 'twitter:card',
           name: 'twitter:card',
           content: 'summary_large_image',
         },
-        { hid: 'og:title', property: 'og:title', content: this.question.body },
+        { hid: 'og:title', property: 'og:title', content: 'dlopp専用質問箱！' },
         {
           hid: 'og:url',
           property: 'og:url',
