5cb255e3d863f00003e2911d4def573b0cd4c74b
Daaiki <daaikii@outlook.jp>
2021-04-08 09:20:50 +0900

    config: :wrench: fontweight400以外アンスト
    ✅ Closes: #47

--- nuxt.config.js
+++ nuxt.config.js
@@ -78,7 +78,7 @@ export default {
       '@nuxtjs/google-fonts',
       {
         families: {
-          'M+PLUS+1p': [100, 300, 400, 500, 700, 800, 900],
+          'M+PLUS+1p': [400],
         },
         display: 'block',
         download: true, // font optimization
